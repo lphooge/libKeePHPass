3838577a7156828678cc91d4351ad9adf8478509
Lutz-Peter Hooge <lphooge@gmx.de>
2012-08-04 01:16:33 +0200

    better example page with datatable

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" charset="utf-8" href="http://ajax.microsoft.com/ajax/jquery.ui/1.8.5/themes/sunny/jquery-ui.css" />
+		<script type="text/javascript" charset="utf-8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" charset="utf-8" src="http://ajax.microsoft.com/ajax/jquery.ui/1.8.5/jquery-ui.min.js"></script>
+ 		<script type="text/javascript" charset="utf-8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.2/jquery.dataTables.min.js"></script>
+		<link rel="stylesheet" type="text/css" charset="utf-8" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.2/css/jquery.dataTables_themeroller.css" />
+ 		
+ 		<script type="text/javascript">
+ 			$(function(){
+  				$('table.kdbtable').dataTable({
+  					"bPaginate": false,
+					"bJQueryUI": true
+				});
+  			});
+  		</script>
+  		<style type="text/css">
+			body.ui-widget{
+				font-size: 13px;
+			}
+  		</style>
+  		<title>libKeePHPass Example Page</title>
 	</head>
-<body>
-<h1>libKeePHPass: Test and Example page</h1>
-<p>
-	Place a file named <code>test.kdb</code> encrypted with the password &quot;<i>test</i>&quot; in this directory to run this test!
-</p>
+	<body class="ui-widget">
+		<h1>libKeePHPass: Test and Example page</h1>
+		<p>
+			Place a file named <code>test.kdb</code> encrypted with the password &quot;<i>test</i>&quot; in this directory to run this test!
+		</p>
 <pre>
 <?php
 try{
@@ -36,66 +57,77 @@ try{
 ?>
 </pre>
 
-<h1>Groups</h1>
-<ul>
-<?foreach($db->getGroupsIndex() as $group): /* @var $group KdbGroup */ ?>
-	<li><?=htmlspecialchars($group->name)?></li>
-<?endforeach?>
-</ul>
-
-<h1>Entries</h1>
-<table border='1'>
-	<tr>
-		<!-- <th>guid</th>  -->
-		<th>name</th>
-		<th>url</th>
-		<th>username</th>
-		<th>password</th>
-		<th>created</th>
-		<th>modified</th>
-		<th>expires</th>
-		<th>notes</th>
-		<th>attachment description</th>
-		<th>attachment</th>
-	</tr>
-	<?foreach($db->getEntriesIndex() as $entry): /* @var $entry KdbEntry */
-		if($entry->isInternal()) // skip internal meta entries
-			continue; 
-		?>
-		<tr>
-			<!-- <td><?=htmlspecialchars($entry->guid)?></td>  -->
-			<td><?=htmlspecialchars($entry->name)?></td>
-			<td><?=htmlspecialchars($entry->url)?></td>
-			<td><?=htmlspecialchars($entry->username)?></td>
-			<td><?=htmlspecialchars($entry->password)?></td>
-			<td><?=date('d.m.Y H:i:s',$entry->creation_time)?></td>
-			<td><?=date('d.m.Y H:i:s',$entry->modification_time)?></td>
-			<td><?=date('d.m.Y H:i:s',$entry->expiration_time)?></td>
-			<td><?=htmlspecialchars($entry->notes)?></td>
-			<td><?=htmlspecialchars($entry->attachment_desc)?></td>
-			<td>
-				<?if($entry->attachment_desc){
-					$ext = pathinfo(trim($entry->attachment_desc), PATHINFO_EXTENSION);
-					$mime_type = null;
-					switch(strtolower($ext)){
-						case 'png':
-							$mime_type = $mime_type?$mime_type:'image/png';
-						case 'jpg':
-						case 'jpeg':
-							$mime_type = $mime_type?$mime_type:'image/jpeg';
-						case 'gif':
-							$mime_type = $mime_type?$mime_type:'image/gif';
-							$datab64 = base64_encode($entry->attachment);
-							echo "<img src='data:$mime_type;base64,$datab64' />";
-							break;
-						default:
-							echo "<pre>".chunk_split(BinStr::toHex($entry->attachment),32)."</pre>";
-					}					
-				}?>
-				
-			</td>
-		</tr>
-	<?endforeach?>
-</table>
-</body>
+		<h1>Groups</h1>
+		<ul>
+		<?foreach($db->getGroupsIndex() as $group): /* @var $group KdbGroup */ ?>
+			<li><?=htmlspecialchars($group->name)?></li>
+		<?endforeach?>
+		</ul>
+		
+		<h1>Entries</h1>
+		<table class="kdbtable">
+			<thead>
+				<tr>
+					<!-- <th>guid</th>  -->
+					<th>Group</th>
+					<th>Name</th>
+					<th>URL</th>
+					<th>Username</th>
+					<th>Password</th>
+					<!-- 
+					<th>created</th>
+					<th>modified</th>
+					 -->
+					<th>expires</th>
+					<th>Notes</th>
+					<th>Attachment</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?foreach($db->getEntriesIndex() as $entry): /* @var $entry KdbEntry */
+				if($entry->isInternal()) // skip internal meta entries
+					continue; 
+				?>
+				<tr>
+					<!-- <td><?=htmlspecialchars($entry->guid)?></td>  -->
+					<td><?=htmlspecialchars($db->getGroupById($entry->group_id)->name)?></td>
+					<td><?=htmlspecialchars($entry->name)?></td>
+					<td><?=htmlspecialchars($entry->url)?></td>
+					<td><?=htmlspecialchars($entry->username)?></td>
+					<td><?=htmlspecialchars($entry->password)?></td>
+					<!--
+					<td><?=date('d.m.Y H:i:s',$entry->creation_time)?></td>
+					<td><?=date('d.m.Y H:i:s',$entry->modification_time)?></td>
+					-->
+					<td><?=$entry->expiration_time?date('d.m.Y H:i:s',$entry->expiration_time):''?></td>
+					<td><?=htmlspecialchars($entry->notes)?></td>
+					<td>
+						<?if($entry->attachment_desc){
+							$ext = pathinfo(trim($entry->attachment_desc), PATHINFO_EXTENSION);
+							$mime_type = null;
+							switch(strtolower($ext)){
+								case 'png':
+									$mime_type = $mime_type?$mime_type:'image/png';
+								case 'jpg':
+								case 'jpeg':
+									$mime_type = $mime_type?$mime_type:'image/jpeg';
+								case 'gif':
+									$mime_type = $mime_type?$mime_type:'image/gif';
+									$datab64 = base64_encode($entry->attachment);
+									echo "<img src='data:$mime_type;base64,$datab64' alt='' title='".htmlspecialchars($entry->attachment_desc)."' />";
+									break;
+								default:
+									$tid = uniqid('tgl');
+									echo '<a style="cursor:pointer;" title="click to toggle data" onclick="$(\'#'.$tid.'\').toggle();">'.htmlspecialchars($entry->attachment_desc).'</a>';
+									echo "<pre class='ui-helper-hidden' id='$tid'>".chunk_split(BinStr::toHex($entry->attachment),32)."</pre>";
+								break;
+							}					
+						}?>
+						
+					</td>
+				</tr>
+			<?endforeach?>
+			</tbody>
+		</table>
+	</body>
 </html>
