2162290f3e6b0b2076671ec0db8abe19c9c11727
Lutz-Peter Hooge <lphooge@gmx.de>
2012-08-06 22:41:54 +0200

    bugfix: time conversion on writing was wrong

--- KdbUtil.php
+++ KdbUtil.php
@@ -24,11 +24,11 @@ class KdbUtil{
 		$dw5 = ord($bintime{4});
 		
 		$year = ($dw1 << 6) | ($dw2 >> 2);
-		$month = (($dw2 & 0x00000003) << 2) | ($dw3 >> 6);
-		$day = ($dw3 >> 1) & 0x0000001F;
-		$hour = (($dw3 & 0x00000001) << 4) | ($dw4 >> 4);
-		$minute = (($dw4 & 0x0000000F) << 2) | ($dw5 >> 6);
-		$second = $dw5 & 0x0000003F;
+		$month = (($dw2 & 0x03) << 2) | ($dw3 >> 6);
+		$day = ($dw3 >> 1) & 0x1F;
+		$hour = (($dw3 & 0x01) << 4) | ($dw4 >> 4);
+		$minute = (($dw4 & 0x0F) << 2) | ($dw5 >> 6);
+		$second = $dw5 & 0x03F;
 		
 		$ts = mktime($hour,$minute,$second,$month,$day,$year);
 		return $ts;
@@ -49,11 +49,11 @@ class KdbUtil{
 		$second = (int) date('s', $time);
 
 		$bytes = str_repeat("\0", 5);
-		$bytes{0} = (($year >> 6) & 0x0000003F);
-		$bytes{1} = ((($year & 0x0000003F) << 2) | (($month >> 2) & 0x00000003));
-		$bytes{2} = ((($month & 0x00000003) << 6) | (($day & 0x0000001F) << 1) | (($hour >> 4) & 0x00000001));
-		$bytes{3} = ((($hour & 0x0000000F) << 4) | (($minute >> 2) & 0x0000000F));
-		$bytes{4} = ((($minute & 0x00000003) << 6) | ($second & 0x0000003F));
+		$bytes{0} = chr(($year >> 6) & 0x3F);
+		$bytes{1} = chr((($year & 0x3F) << 2) | (($month >> 2) & 0x03));
+		$bytes{2} = chr((($month & 0x03) << 6) | (($day & 0x1F) << 1) | (($hour >> 4) & 0x01));
+		$bytes{3} = chr((($hour & 0x0F) << 4) | (($minute >> 2) & 0x0F));
+		$bytes{4} = chr((($minute & 0x03) << 6) | ($second & 0x3F));
 	
 		return $bytes;
 	}
